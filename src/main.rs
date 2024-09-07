1f5eae87a2c6447d8af5af2856b80fa0e6b539ba
Limbert Lino <limbertlino@gmail.com>
2024-09-06 22:57:07 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,130 +2,56 @@
 extern crate rocket;
 
 use reqwest::Error;
+use rocket::serde::json::Json;
 use rocket::{
     http::Status,
     serde::json::{json, Value},
 };
 
-async fn get_issuance_invitation_code() -> Result<Value, Error> {
+use std::fs;
+
+// use serde_json::Value;
+
+use rocket::serde::{Deserialize, Serialize};
+
+// enum Type {
+//     Standard,
+//     Vip,
+//     Extra,
+//     Fast,
+// }
+
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Ticket {
+    name: String,
+    membership: String,
+    period: String,
+}
+
+async fn get_issuance_invitation_code(_credential_data: Value) -> Result<Value, Error> {
     let client = reqwest::Client::new();
     let base_url = "https://sandbox-ssi.extrimian.com/v1/credentialsbbs/wacioob";
 
-    let json_data = r##"{
-  "did": "did:quarkid:EiD-CWTXMNUwg1Yp-k2AviPN17INFEnwG4cMA7BS4yLHxw",
-  "oneTimeUse": false,
-  "vc": {
-    "@context": [
-      "https://w3id.org/security/bbs/v1",
-      "https://www.w3.org/2018/credentials/v1",
-      "https://limbertlino.github.io/schemas/passport.json"
-    ],
-    "id": "17253638365188",
-    "type": ["VerifiableCredential", "TuristCredential"],
-    "issuer": {
-      "id": "did:quarkid:EiD-CWTXMNUwg1Yp-k2AviPN17INFEnwG4cMA7BS4yL345",
-      "name": "Club tech"
-    },
-    "issuanceDate": "Tue Sep 03 2024 09:02:03 GMT-0400 (Atlantic Standard Time)",
-    "expirationDate": "2025-08-31",
-    "credentialSubject": {
-      "name": "Limbert Lino Mattos",
-      "passport": "B-43214583",
-      "period": "2024-08-31 - 2025-08-31",
-      "gender": "female"
-    }
-  },
-  "outputDescriptor": {
-    "id": "turist-output",
-    "schema": "turist-output",
-    "display": {
-      "title": {
-        "text": "Club Tech"
-      },
-      "subtitle": {
-        "text": "Credencial de socio"
-      },
-      "description": {
-        "text": "Con tu credencial de tech puedes obtener descuentos y acceder a nuestr"
-      },
-      "properties": [
-        {
-          "label": "Nombre",
-          "path": ["$.credentialSubject.name"],
-          "schema": {
-            "type": "string"
-          },
-          "fallback": "Name"
-        },
-        {
-          "label": "Número de pasaporte",
-          "path": ["$.credentialSubject.passport"],
-          "schema": {
-            "type": "string"
-          },
-          "fallback": "Passport"
-        },
-        {
-          "label": "Período de estadía",
-          "path": ["$.credentialSubject.period"],
-          "schema": {
-            "type": "string"
-          },
-          "fallback": "Period"
-        },
-        {
-          "label": "Genero",
-          "path": ["$.credentialSubject.gender"],
-          "schema": {
-            "type": "string"
-          },
-          "fallback": "Genero"
-        }
-      ]
-    },
+    // println!("{credential_data}");
 
-    "styles": {
-      "thumbnail": {
-        "uri": "https://cdn.dribbble.com/users/2068059/screenshots/4456420/tech_logo.png",
-        "alt": "Logo"
-      },
-      "hero": {
-        "uri": "https://i.ibb.co/bHF3g7m/Credencial-Hack-Along-1.png",
-        "alt": "Background"
-      },
-      "background": {
-        "color": "#FFFFFF"
-      },
-      "text": {
-        "color": "#2B2B2D"
-      }
-    }
-  },
-  "issuer": {
-    "name": "Club tech de Limbert",
-    "styles": {
-      "thumbnail": {
-        "uri": "https://cdn.dribbble.com/users/2068059/screenshots/4456420/tech_logo.png",
-        "alt": "Logo"
-      },
-      "hero": {
-        "uri": "https://i.ibb.co/bHF3g7m/Credencial-Hack-Along-1.png",
-        "alt": "Background"
-      },
-      "background": {
-        "color": "#FFFFFF"
-      },
-      "text": {
-        "color": "#2B2B2D"
-      }
-    }
-  }
-}"##;
+    // y si paso este archivo directamente como Value?
+    let string_json_data = fs::read_to_string("json_model/base_ticket_template.json")
+        .expect("Can't read the json file");
+
+    // let mut json_value = json!(string_json_data);
+
+    let mut json_value: Value =
+        serde_json::from_str(&string_json_data).expect("JSON was not well-formatted");
+
+    json_value["vc"]["credentialSubject"]["name"] = json!("nuevo nombre");
+
+    println!("{json_value:#?}");
 
     let request_response = client
         .put(base_url)
         .header("Content-type", "application/json")
-        .body(json_data.to_owned())
+        .body(string_json_data.to_owned())
         .send()
         .await;
 
@@ -147,9 +73,11 @@ fn ping() -> &'static str {
     "pong!"
 }
 
-#[put("/issue-vc", format = "json")]
-async fn create_new_vc() -> Result<Value, Status> {
-    match get_issuance_invitation_code().await {
+#[put("/issue-vc", format = "json", data = "<ticket>")]
+async fn create_new_vc(ticket: Json<Ticket>) -> Result<Value, Status> {
+    let credential_data = json!(ticket.0);
+
+    match get_issuance_invitation_code(credential_data).await {
         Ok(json) => Ok(json!(json)),
         Err(_) => Err(Status::InternalServerError),
     }
